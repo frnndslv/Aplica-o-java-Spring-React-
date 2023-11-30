a0c1ddf5f61dc6b9c00cb98fd179b099ac2d08a6
fernanda.costa <fernandinhacosta.97@hotmail.com>
2023-11-30 17:07:42 -0300

    Primeira parte back

---
+++ src/main/java/com/example/cardapio/oxentesushi/controller/foodController.java
@@ -0,0 +1,27 @@
+package com.example.cardapio.oxentesushi.controller;
+
+import com.example.cardapio.oxentesushi.food.Food;
+import com.example.cardapio.oxentesushi.food.FoodRepository;
+import com.example.cardapio.oxentesushi.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+    @RequestMapping("food")
+    public class foodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+
+    }
+  }
